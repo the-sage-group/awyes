30dc587a251dabe909681978a048813f09b18cee
trumanpurnell <truman.purnell@gmail.com>
2024-11-13 00:42:37 -0700

    The begginnings of register!

--- awyes.ts
+++ awyes.ts
@@ -1,43 +1,37 @@
 import express from "express";
-import { typeOf } from "@deepkit/type";
+import { App } from "@deepkit/app";
+import { validate } from "@deepkit/type";
 
-export class Node<K extends PropertyKey, T extends (...args: any[]) => any> {
-  id: K;
-  action: T;
-  declare result: ReturnType<T>;
+import * as types from "./types";
 
-  constructor(id: K, action: T) {
-    this.id = id;
-    this.action = action;
-  }
-}
-
-export class Flow<T extends object> {
-  nodes: Node<any, any>[];
-  declare context: T;
-
-  constructor(nodes: Node<any, any>[] = []) {
-    this.nodes = nodes;
-  }
-
-  add<K extends PropertyKey, V>(node: Node<K, (flow: Flow<T>) => V>) {
-    return new Flow<{
-      [P in K | keyof T]: P extends keyof T ? T[P] : V;
-    }>([...this.nodes, node] as any);
-  }
-}
-
-type Workflow = (...args: any[]) => Flow<any>;
-
-export function serve(...workflows: Workflow[]) {
-  const app = express();
-  const port = 3000;
+export * from "./types";
+export function register(...workflows: types.Workflow<any>[]) {
+  const capp = new App({});
+  const eapp = express();
+  eapp.use(express.json());
 
   workflows.forEach((flow) => {
-    console.log(typeOf<typeof flow>());
+    console.log(
+      validate<Parameters<typeof flow>[number]>({
+        domainName: "string",
+        subDomains: ["string"],
+      })
+    );
+
+    capp.command(flow.name, () => {
+      console.log("Whoa");
+    });
+
+    eapp.get(`/${flow.name}`, (req, res) => {
+      res.send(flow({}));
+    });
+
+    eapp.post(`/${flow.name}`, (req, res) => {
+      console.log(req.body);
+      res.send(req.body);
+    });
   });
 
-  // app.listen(port, () => {
-  //   console.log(`Example app listening on port ${port}`);
-  // });
+  capp.run();
+  // eapp.listen(3000, () => {});
 }
---
+++ types.ts
@@ -0,0 +1,27 @@
+export class Node<K extends PropertyKey, T extends (...args: any[]) => any> {
+  id: K;
+  action: T;
+  declare result: ReturnType<T>;
+
+  constructor(id: K, action: T) {
+    this.id = id;
+    this.action = action;
+  }
+}
+
+export class Flow<T extends object> {
+  nodes: Node<any, any>[];
+  declare context: T;
+
+  constructor(nodes: Node<any, any>[] = []) {
+    this.nodes = nodes;
+  }
+
+  add<K extends PropertyKey, V>(node: Node<K, (flow: Flow<T>) => V>) {
+    return new Flow<{
+      [P in K | keyof T]: P extends keyof T ? T[P] : V;
+    }>([...this.nodes, node] as any);
+  }
+}
+
+export type Workflow<T extends object> = (params: T) => Flow<any>;
