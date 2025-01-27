e9495a4d05543bd03dcd259bcd445966e82e6653
trumanpurnell <truman.purnell@gmail.com>
2025-01-26 23:52:55 -0700

    Fix up client exports for node and web

--- clients/node/src/index.ts
+++ clients/node/src/index.ts
@@ -1,20 +1,7 @@
-export type {
-  Node,
-  Flow,
-  Edge,
-  Event,
-  RegisterNodeRequest,
-  RegisterNodeResponse,
-  ListNodesRequest,
-  ListNodesResponse,
-} from "./generated/proto/awyes";
-
-export { AwyesClient, EventType, Label } from "./generated/proto/awyes";
-export { credentials, Metadata } from "@grpc/grpc-js";
-
+export * from "./generated/proto/awyes";
 export {
-  FieldDescriptorProto_Type,
-  FieldDescriptorProto_Label,
+  FieldDescriptorProto_Type as Type,
+  FieldDescriptorProto_Label as Label,
 } from "./generated/google/protobuf/descriptor";
 
-export type { Value } from "./generated/google/protobuf/struct";
+export { credentials, Metadata } from "@grpc/grpc-js";
--- clients/web/src/index.ts
+++ clients/web/src/index.ts
@@ -1,27 +1,5 @@
 // Export all generated types and interfaces
-export type {
-  Node,
-  Edge,
-  Flow,
-  Journey,
-  Event,
-  RegisterFlowRequest,
-  RegisterFlowResponse,
-  RegisterNodeRequest,
-  RegisterNodeResponse,
-  ExecuteFlowRequest,
-  ExecuteFlowResponse,
-  ListNodesRequest,
-  ListNodesResponse,
-  ListFlowsRequest,
-  ListFlowsResponse,
-} from "./generated/awyes";
-export { EventType } from "./generated/awyes";
-
-export {
-  FieldDescriptorProto_Type,
-  FieldDescriptorProto_Label,
-} from "./generated/google/protobuf/descriptor";
-export type { FieldDescriptorProto } from "./generated/google/protobuf/descriptor";
-
-export { AwyesClient } from "./generated/awyes.client";
+export * from "./generated/awyes";
+export * from "./generated/awyes.client";
+export * from "./generated/google/protobuf/descriptor";
+export * from "./generated/google/protobuf/struct";
